94af1b0ded39958e4cd1cd3b881134eebc8046fe
Alamag <artem.baev.2001@yandex.ru>
2021-12-11 13:33:50 +0300

    Initial commit

---
+++ app/src/main/java/bonch/hack/ssd/simplary/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package bonch.hack.ssd.simplary.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
