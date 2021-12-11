0cfaff4505cbea20bdfcc5cdb0eab8bf7d465ed9
waqeen <a.zhdanuhin@mail.ru>
2021-12-11 18:26:09 +0300

    Добавил недостающие файлы

--- app/src/main/java/bonch/hack/ssd/simplary/ui/notes/NotesViewModel.kt
+++ app/src/main/java/bonch/hack/ssd/simplary/ui/notes/NotesViewModel.kt
@@ -1,10 +1,10 @@
-package bonch.hack.ssd.simplary.ui.dashboard
+package bonch.hack.ssd.simplary.ui.notes
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class NotesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
