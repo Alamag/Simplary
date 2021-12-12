1a03a4b16f5c8effb62da5dea1180378757fca03
Sery0077 <sery.vlasenko.ru@gmail.com>
2021-12-12 12:06:16 +0300

    Добавлены вкладки в комьюнити

---
+++ app/src/main/java/bonch/hack/ssd/simplary/ui/community/tabs/popular/PopularAdapter.kt
@@ -0,0 +1,24 @@
+package bonch.hack.ssd.simplary.ui.community.tabs.popular
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import bonch.hack.ssd.simplary.databinding.ItemPostBinding
+
+class PopularAdapter : RecyclerView.Adapter<PopularAdapter.PostVH>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostVH {
+        return PostVH(
+            ItemPostBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: PostVH, position: Int) {}
+
+    override fun getItemCount(): Int = 3
+
+    inner class PostVH(private val binding: ItemPostBinding) : RecyclerView.ViewHolder(binding.root)
+}
---
+++ app/src/main/java/bonch/hack/ssd/simplary/ui/community/tabs/subscriptions/FragmentSubscriptions.kt
@@ -0,0 +1,35 @@
+package bonch.hack.ssd.simplary.ui.community.tabs.subscriptions
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import bonch.hack.ssd.simplary.R
+import bonch.hack.ssd.simplary.databinding.FragmentCommunityTabBinding
+import bonch.hack.ssd.simplary.ui.community.tabs.popular.PopularAdapter
+
+class FragmentSubscriptions: Fragment(R.layout.fragment_community_tab) {
+
+    private var _binding: FragmentCommunityTabBinding? = null
+    private val binding get() = _binding!!
+
+    private val adapter: PopularAdapter by lazy { PopularAdapter() }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCommunityTabBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.recyclerCommunity.adapter = adapter
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
